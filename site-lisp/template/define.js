89d1af61dd4734e20530f63bc3b68d51e3614b49
okapi <jf@geonef.fr>
2012-10-23 18:42:39 +0200

    updated templates

--- site-lisp/template/define.js
+++ site-lisp/template/define.js
@@ -4,11 +4,21 @@
  * Description plus longue du module (conseillé)
  */
 define([
+  "module",
   "dojo/_base/declare",
   "dojo/_base/lang"
-], function(declare, lang) {
+], function(module, declare, lang) {
 
 return declare([_Widget], { //--noindent--
+
+  someMethod: function() {
+
+  },
+
+  /**
+   * The Class name comes from module's -- used by declare()
+   */
+  declaredClass: module.id
 
 });
 
--- site-lisp/template/widget.js
+++ site-lisp/template/widget.js
@@ -4,25 +4,40 @@
  * Description plus longue du module (conseillé)
  */
 define([
+  "module",
   "dojo/_base/declare",
   "geonef/jig/_Widget",
   "dojo/_base/lang",
   "geonef/button/Action"
-], function(declare, _Widget, lang, Action) {
+], function(module, declare, _Widget, lang, Action) {
 
 return declare([_Widget], { //--noindent--
 
+  /**
+   * @override
+   */
   "class": _Widget.prototype["class"] + " myWidget",
 
+  /**
+   * @override
+   */
   makeContentNodes: function() {
     return [
       ["div", {}, "My content"]
     ];
   },
 
+  /**
+   * @override
+   */
   startup: function() {
     this.inherited(arguments);
-  }
+  },
+
+  /**
+   * The Class name comes from module's -- used by declare()
+   */
+  declaredClass: module.id
 
 });
 
